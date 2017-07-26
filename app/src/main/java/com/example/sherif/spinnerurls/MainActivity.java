a7f79a19b0d329421787034225222129fdb4f64e
sherif <sherifsalem82@gmail.com>
2017-07-27 00:36:25 +0200

    add webView setWebClietn method to the onItemSelected

--- app/src/main/java/com/example/sherif/spinnerurls/MainActivity.java
+++ app/src/main/java/com/example/sherif/spinnerurls/MainActivity.java
@@ -1,29 +1,44 @@
 package com.example.sherif.spinnerurls;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-import java.net.URL;
-
 public class MainActivity extends AppCompatActivity {
-    URL[] list;
-
+    String[] list = {"GOOGLE", "FACEBOOK"};
+    Spinner spnr;
+    WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Spinner spnr;
 
+        webView =(WebView) findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("www.google.com");
         spnr = (Spinner)findViewById(R.id.spinner);
-        ArrayAdapter adapter = new ArrayAdapter<URL>(this, R.layout.support_simple_spinner_dropdown_item,list);
+
+
+
+        ArrayAdapter adapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item,list);
         spnr.setAdapter(adapter);
         spnr.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                webView.setWebViewClient(new WebViewClient() {
+                    @Override
+                    public boolean shouldOverrideUrlLoading (WebView view, String url){
+                        view.loadUrl(url);
+                        return true;
+
+                    }
+                });
 
             }
 
